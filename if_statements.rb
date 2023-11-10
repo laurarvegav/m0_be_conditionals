32380cfd2b5cb3e667797215388b88d544910a25
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-11-10 15:12:13 -0700

    Add code to evaluate if the ingredients are enough to make pizza

--- if_statements.rb
+++ if_statements.rb
@@ -75,5 +75,12 @@ end
 # Experiment with manipulating the value held within both variables
 # to make sure all above conditions output what you expect.
 
+ 
 cups_of_flour = 1
 has_sauce = true
+
+if cups_of_flour >=2 && has_sauce == true
+  puts "I can make pizza"
+else
+  puts "I cannot make pizza"
+end
