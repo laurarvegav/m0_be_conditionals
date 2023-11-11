6bc2a36ee8530b9fd13159dffe55bfaa6659abfb
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-11-10 19:23:40 -0700

    Comment why the first idea did not work

--- code_challenges/spicy_challenge_lrvv.rb
+++ code_challenges/spicy_challenge_lrvv.rb
@@ -6,16 +6,18 @@
 # of both 3 and 5, print "FizzBuzz". If the Integer is not a 
 # multiple of either, print the Integer itself.
 
-number_strikes = 30
+number_strikes = 3
 strike_one = "Fizz"
 strike_two = "Buzz"
 # number_strikes will be a multiple of 3 when the result of 
-# numer_strikes divided in 3 is an integer. 
+# numer_strikes divided in 3 is an integer. First Idea
 
-if (number_strikes/3).class == Integer && (number_strikes/5).class == Integer
-    puts "#{strike_one}#{strike_two}" 
-  elsif (number_strikes/3).class == Integer 
-      puts strike_one
-  elsif (number_strikes/5).class == Integer 
-      puts strike_two
-  end
+# if ((number_strikes/3).class == Integer) && ((number_strikes/5).class == Integer)
+#     puts "#{strike_one}#{strike_two}" 
+#   elsif (number_strikes/3).class == Integer 
+#       puts strike_one
+#   elsif (number_strikes/5).class == Integer 
+#       puts strike_two
+#   end
+#   puts (number_strikes/5).class
+# Did not work, when ruby divides it does not show the decimals.
